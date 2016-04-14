bdaa1438d2b4490e50bfcdce8eb76454140e2eaa
Dan Cook <danielecook@gmail.com>
2016-04-14 13:11:26 -0500

    remove examples

--- R/main.R
+++ R/main.R
@@ -150,9 +150,6 @@ authenticate_datastore <- function(key, secret, project) {
 #' @param id The entity id.
 #' @param project Google cloud platform project id/name.
 #'
-#' @examples
-#' lookup("test", "m")
-#'
 #' @seealso \url{https://cloud.google.com/datastore/docs/concepts/entities} - Entities, Properties, and Keys
 #' @return dataframe of entity.
 #' @export
@@ -205,8 +202,6 @@ lookup <- function(kind, name = NULL, id = NULL) {
 #' @param ... Additional arguments to store as properties.
 #' @param mutation_type The type of mutation. One of \strong{insert}, \strong{update}, \strong{upsert}, or \strong{delete}. Default is upsert.
 #'
-#' @examples
-#' commit("test", "entity_name", int_column = as.integer(1), str_column = "awesome!", float_column = 3.14)
 #'
 #' @return transaction_id
 #'
