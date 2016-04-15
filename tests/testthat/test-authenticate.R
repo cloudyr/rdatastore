3c6f746e83ad67595deaa44d2eb53d5f1cc5a12b
Daniel Cook <danielecook@gmail.com>
2016-04-14 21:20:28 -0500

    test no ex.

--- tests/testthat/test-authenticate.R
+++ tests/testthat/test-authenticate.R
@@ -1,9 +1,9 @@
 
-
 client_secret <- paste0(find.package("rdatastore"), "/client-secret.json")
 authenticate_datastore_service(client_secret, "andersen-lab")
+print(dir("/home/travis/build/danielecook/rdatastore/rdatastore.Rcheck/"))
 
-test_that("authenticate", {
+test_that("is true true?", {
   x <- TRUE
   expect_equal(TRUE, TRUE)
 })
