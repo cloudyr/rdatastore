8e18eb90062191bf10e753e5745228b32b9f6967
Daniel Cook <danielecook@gmail.com>
2016-04-14 20:23:41 -0500

    fix authenticate

--- tests/testthat/test-authenticate.R
+++ tests/testthat/test-authenticate.R
@@ -1,6 +1,6 @@
 
-
-authenticate_datastore_service("client-secret.json", "andersen-lab")
+client_secret <- paste0(find.package("rdatastore"), "/client-secret.json")
+authenticate_datastore_service(client_secret, "andersen-lab")
 
 test_that("authenticate", {
   x <- TRUE
