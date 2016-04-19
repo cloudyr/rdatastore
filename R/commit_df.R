897bd8794e56dda6e5d30ddabaf7d6c308bcd28c
Daniel Cook <danielecook@gmail.com>
2016-04-19 07:34:32 -0500

    fix commit_df

--- R/commit_df.R
+++ R/commit_df.R
@@ -23,22 +23,24 @@ commit_df <- function(kind = NULL, name = NULL, df, mutation_type = "upsert", ke
   if (is.null(kind)) {
     kind <- deparse(substitute(kind))
   }
-
   # Maximum of 25 items can be added at once.
   df_groups <- split(df, (as.numeric(rownames(df))-1) %/% 25)
 
   sapply(df_groups, function(df) {
-    print(df)
 
     transaction_id <- transaction()
 
     mutation = list()
 
-    keep_cols = which(!(names(df) == name))
+    if (!is.null(name)) {
+      keep_cols = which(!(names(df) == name))
+    } else {
+      keep_cols = 1:length(df)
+    }
 
     data <- lapply(1:nrow(df), function(i) {
       # If name is null, autoallocate id.
-      if (!is.null(df[i,name])) {
+      if (!is.null(name)) {
         path_item <- list(
           kind = kind,
           name = df[i,name]$name
@@ -58,7 +60,7 @@ commit_df <- function(kind = NULL, name = NULL, df, mutation_type = "upsert", ke
       c(list("upsert" = key_obj))
     })
 
-     body <- list(mutations = c(data[1:25]),
+     body <- list(mutations = c(data[1:nrow(df)]),
                  transaction = transaction_id
     )
 
@@ -66,8 +68,7 @@ commit_df <- function(kind = NULL, name = NULL, df, mutation_type = "upsert", ke
     req <- httr::POST(paste0(rdatastore_env$url, ":commit"),
                       httr::config(token = rdatastore_env$token),
                       body =  body,
-                      encode = "json",
-                      verbose())
+                      encode = "json")
 
 
     # Return transaction id if successful, else error.
